c5b83cebee210b1a31a1e040f3a0f1875a239f9d
정광철 <jung.kc@huray.net>
2023-10-23 22:10:48 +0900

    [ADD] custom exception 수정

--- src/main/java/com/example/chorushop/common/exception/RestApiErrorResponse.java
+++ src/main/java/com/example/chorushop/common/exception/RestApiErrorResponse.java
@@ -4,16 +4,13 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.core.SpringVersion;
 
 @Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RestApiErrorResponse {
 
     private RestApiExceptionCode code;
-
     private String message;
-
 
 }
--- src/main/java/com/example/chorushop/common/exception/RestApiExceptionHandler.java
+++ src/main/java/com/example/chorushop/common/exception/RestApiExceptionHandler.java
@@ -1,9 +1,7 @@
 package com.example.chorushop.common.exception;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,9 +13,7 @@ public class RestApiExceptionHandler {
 
     @ExceptionHandler(RestApiException.class)
     public ResponseEntity<RestApiErrorResponse> handleResponseStatusException(final RestApiException ex) {
-
-        RestApiErrorResponse restApiErrorResponse = new RestApiErrorResponse();
-
+        RestApiErrorResponse restApiErrorResponse = new RestApiErrorResponse(ex.getCode(), ex.getMessage());
         return new ResponseEntity<>(restApiErrorResponse, ex.getCode().getStatus());
     }
 }
